8a35bb703eac12f6d8b8444e677a811986cc5bf3
Ashleigh Kaffenberger <akaffenberger@users.noreply.github.com>
2021-04-12 19:56:39 -0400

    Fixed issue with loading spec via nelmio factory

--- Schema/Factory/NelmioFactory.php
+++ Schema/Factory/NelmioFactory.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Cydrickn\OpenApiValidatorBundle\Schema\Factory;
 
+use cebe\openapi\ReferenceContext;
+use Cydrickn\OpenApiValidatorBundle\Schema\Schema;
 use League\OpenAPIValidation\PSR7\SchemaFactory;
 use Psr\Container\ContainerInterface;
-use Cydrickn\OpenApiValidatorBundle\Schema\Schema;
 use Psr\Log\LoggerInterface;
 
 class NelmioFactory implements SchemaFactory
@@ -23,16 +24,23 @@ class NelmioFactory implements SchemaFactory
     public function createSchema(): Schema
     {
         $apiDoc = $this->generatorLocator->get('default');
-        $spec = $apiDoc->generate()->toArray();
-        if ($spec['swagger']) {
+        $spec = $apiDoc->generate();
+
+        if (method_exists($spec,'toArray')) {
+            $spec = $spec->toArray();
+        } else {
+            $spec = json_decode(json_encode($spec), true);
+        }
+
+        if (!empty($spec['swagger'])) {
             $this->logger->warning(
                 'Validating of request/response will be ignored due to the Open API Spec'
                 . ' Version 2 and below are not supported.'
             );
         }
         $schema = new Schema($spec);
-        $schema->resolveReferences();
-
+        $context = new ReferenceContext($schema, '://');
+        $schema->resolveReferences($context);
         return $schema;
     }
 }
